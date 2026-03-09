8d5884e3aa30a743a410286e16a1c46e1388fcaa
Chris Turner <peavers@gmail.com>
2026-03-08 22:03:10 -0500

    BiS data update: ArchonMythicDB (MYTHIC_PLUS)

--- src/Data/ArchonMythicDB.lua
+++ src/Data/ArchonMythicDB.lua
@@ -2,7 +2,7 @@ local addonName, addonTable = ...
 addonTable.ArchonMythicDB = addonTable.ArchonMythicDB or {}
 
 local bisData = {
-	updated = "2026-03-08 03:03:05",
+	updated = "2026-03-09 03:03:08",
 
 	[1] = {
 		specs = {
