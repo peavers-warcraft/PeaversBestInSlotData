28635efc5e401536041af5928c2a25cda88513a4
Chris Turner <peavers@gmail.com>
2026-03-11 22:03:23 -0500

    BiS data update: wowcompareRaidDB (RAID)

--- src/Data/WowCompareRaidDB.lua
+++ src/Data/WowCompareRaidDB.lua
@@ -2,7 +2,7 @@ local addonName, addonTable = ...
 addonTable.WowCompareRaidDB = addonTable.WowCompareRaidDB or {}
 
 local bisData = {
-	updated = "2026-03-11 03:03:13",
+	updated = "2026-03-12 03:03:21",
 
 	[1] = {
 		specs = {
