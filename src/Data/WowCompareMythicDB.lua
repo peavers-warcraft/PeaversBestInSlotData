939b2d0097d5b4540c76309fbe3d73de5476903b
Chris Turner <peavers@gmail.com>
2026-03-14 22:03:10 -0500

    BiS data update: wowcompareMythicDB (MYTHIC_PLUS)

--- src/Data/WowCompareMythicDB.lua
+++ src/Data/WowCompareMythicDB.lua
@@ -2,7 +2,7 @@ local addonName, addonTable = ...
 addonTable.WowCompareMythicDB = addonTable.WowCompareMythicDB or {}
 
 local bisData = {
-	updated = "2026-03-14 03:03:18",
+	updated = "2026-03-15 03:03:08",
 
 	[1] = {
 		specs = {
